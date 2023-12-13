6762391723cc4dc5de11063ad30886a175e854e1
aradhya-10 <202051034@iiitvadodara.ac.in>
2023-12-13 23:44:20 +0530

    UC1: Perfom IO Operations

---
+++ src/main/java/com/employee/entity/EmployeePayrollData.java
@@ -0,0 +1,12 @@
+package com.employee.entity;
+public class EmployeePayrollData{
+    int id;
+    String name;
+    double salary;
+
+    public EmployeePayrollData(int id, String name, double salary){
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+}
